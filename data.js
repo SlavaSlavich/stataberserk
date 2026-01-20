50b58f756b5bb747e257d4d1eeee1814264998b3
SlavaSlavich <113182038+SlavaSlavich@users.noreply.github.com>
2026-01-20 10:05:08 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+const LIVE_MATCHES = [];
+
+const MATCH_HISTORY = [
+    {
+        "id": "Raze_vs_Havoc_restored",
+        "team1": "Raze",
+        "team2": "Havoc",
+        "score": "13-9",
+        "winner": "team1",
+        "start_time_str": "20.01 09:10",
+        "time": "20.01 09:10",
+        "league": "CS2 1x1 Berserk League",
+        "logos": {
+            "t1": "img/raze.png",
+            "t2": "img/havoc.png"
+        },
+        "odds": {
+            "p1": "1.85",
+            "p2": "1.85"
+        },
+        "map_scores": {
+            "map_1": "13-9"
+        }
+    }
+];
